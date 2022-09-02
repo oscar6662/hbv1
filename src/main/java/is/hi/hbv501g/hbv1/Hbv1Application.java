5133c26a183143ae89fbbc87556baf9262b371b6
bjarnig86 <72604468+bjarnig86@users.noreply.github.com>
2022-09-02 17:25:58 +0000

    initial commit

--- src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
+++ src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
@@ -2,8 +2,9 @@ package is.hi.hbv501g.hbv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Hbv1Application {
 
     public static void main(String[] args) {
