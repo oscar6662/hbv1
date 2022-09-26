39258995af5de03e9baeab81a3b791a344ecbbf3
bjarnig86 <72604468+bjarnig86@users.noreply.github.com>
2022-09-26 20:12:37 +0000

    filling in the entities classes

---
+++ src/main/java/is/hi/hbv501g/hbv1/Persistence/Entities/DaycareWorker.java
@@ -0,0 +1,15 @@
+package is.hi.hbv501g.hbv1.Persistence.Entities;
+
+import java.util.UUID;
+
+public class DaycareWorker {
+    private UUID id;
+    private String ssn;
+    private String fullName;
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private Child[] children;
+    private int experienceInYears;
+    private String address;
+}
---
+++ src/main/java/is/hi/hbv501g/hbv1/Persistence/Entities/Parent.java
@@ -0,0 +1,14 @@
+package is.hi.hbv501g.hbv1.Persistence.Entities;
+
+import java.util.UUID;
+
+public class Parent {
+    private UUID id;
+    private String ssn;
+    private String fullName;
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private Child[] children;
+
+}
