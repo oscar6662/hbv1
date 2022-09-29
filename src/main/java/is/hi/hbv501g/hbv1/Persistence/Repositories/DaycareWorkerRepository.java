b9c690e0d773681d650b140985b00e48a3730c6d
Dagbjort <64809770+Dagbjort@users.noreply.github.com>
2022-09-29 10:29:48 +0000

    Added DaycareWorkerRepository. Needs changes

--- src/main/java/is/hi/hbv501g/hbv1/Persistence/Repositories/DaycareWorkerRepository.java
+++ src/main/java/is/hi/hbv501g/hbv1/Persistence/Repositories/DaycareWorkerRepository.java
@@ -1,18 +1,22 @@
-package is.hi.hbv501g.hbv1.Services;
+package is.hi.hbv501g.hbv1.Persistence.Repositories;
 
 import is.hi.hbv501g.hbv1.Persistence.Entities.DaycareWorker;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Book;
 import java.util.List;
 import java.util.UUID;
 
-public interface DaycareWorkerService {
+public interface DaycareWorkerRepository extends JpaRepository <DaycareWorker, UUID> {
+
     List<DaycareWorker> findByLocation(String location);
     List<DaycareWorker> findByLocationCode(int locationCode);
-    DaycareWorker findById(UUID id);
+
+    //DaycareWorker findById(UUID id);
+    //strange error with the UUID that i cannot seem to resolve
+
     List<DaycareWorker> findAll();
     DaycareWorker addDaycareWorker(DaycareWorker daycareWorker);
     void delete(DaycareWorker daycareWorker);
-}
 
+}
 
