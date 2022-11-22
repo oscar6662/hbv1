362a92c16c59458cbb2c8a51a0d286f7344f0274
Óskar Helgi <16443844+oscar6662@users.noreply.github.com>
2022-11-22 16:09:14 +0100

    main: login

--- src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
+++ src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
@@ -23,12 +24,11 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors();
-        http.csrf().disable();
+        http.cors().and().csrf().disable();
         http.authorizeRequests()
                 .mvcMatchers("/", "/api/daycareworkers", "/api/addlocation", "/api/locations", "/api/adddaycareworker", "/api/isauthenticated", "/api/createparent", "/api/daycareworkerexists/{ssn}", "/api/parentexists/{ssn}").permitAll() // allow all users to access the home pages and the static images directory
                 .anyRequest().authenticated() // all other requests must be authenticated
-                .and().oauth2Login().defaultSuccessUrl("http://localhost:5173/")
+                .and().oauth2Login()//.defaultSuccessUrl("http://localhost:5173/")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // handle logout requests at /logout path
                 .addLogoutHandler(logoutHandler); // customize logout handler to log out of Auth0
@@ -53,8 +53,8 @@ public class SecurityConfig {
 }
 
 @Configuration
+@EnableWebMvc
 class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
