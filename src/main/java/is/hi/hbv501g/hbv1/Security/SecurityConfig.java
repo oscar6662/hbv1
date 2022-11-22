045aa89505561b6991f4bec64ab9c4d6105996d8
Óskar Helgi <16443844+oscar6662@users.noreply.github.com>
2022-11-22 17:01:51 +0100

    test

--- src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
+++ src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         http.authorizeRequests()
                 .mvcMatchers("/", "/api/daycareworkers", "/api/addlocation", "/api/locations", "/api/adddaycareworker", "/api/isauthenticated", "/api/createparent", "/api/daycareworkerexists/{ssn}", "/api/parentexists/{ssn}").permitAll() // allow all users to access the home pages and the static images directory
                 .anyRequest().authenticated() // all other requests must be authenticated
-                .and().oauth2Login()//.defaultSuccessUrl("http://localhost:5173/")
+                .and().oauth2Login().defaultSuccessUrl("https://hbv1-framendi.herokuapp.com/")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // handle logout requests at /logout path
                 .addLogoutHandler(logoutHandler); // customize logout handler to log out of Auth0
