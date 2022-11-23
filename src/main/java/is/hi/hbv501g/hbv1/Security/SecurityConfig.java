0721cc60c8be3ed04fc7d8316927baac7ffadace
Óskar Helgi <16443844+oscar6662@users.noreply.github.com>
2022-11-23 19:16:33 +0100

    test

--- src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
+++ src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
@@ -107,38 +107,32 @@ class WebConfig implements WebMvcConfigurer {
         private static final String REACT_STATIC_DIR = "static";
 
         private Resource index = new ClassPathResource(REACT_DIR + "index.html");
-        private List<String> rootStaticFiles = Arrays.asList("favicon.io",
-                "asset-manifest.json", "manifest.json", "service-worker.js");
 
-        @Override
-        public Resource resolveResource(HttpServletRequest request, String requestPath,
-                                        List<? extends Resource> locations, ResourceResolverChain chain) {
-            return resolve(requestPath, locations);
-        }
 
-        @Override
-        public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
-            Resource resolvedResource = resolve(resourcePath, locations);
-            if (resolvedResource == null) {
-                return null;
-            }
-            try {
-                return resolvedResource.getURL().toString();
-            } catch (IOException e) {
-                return resolvedResource.getFilename();
-            }
-        }
-
-        private Resource resolve(String requestPath, List<? extends Resource> locations) {
-
-            if (requestPath == null) return null;
-
-            if (rootStaticFiles.contains(requestPath)
-                    || requestPath.startsWith(REACT_STATIC_DIR)) {
-                return new ClassPathResource(REACT_DIR + requestPath);
-            } else
+            @Override
+            public Resource resolveResource(HttpServletRequest request, String requestPath,
+                                            List<? extends Resource> locations, ResourceResolverChain chain) {
                 return index;
-        }
+            }
+
+            @Override
+            public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
+                Resource resolvedResource = resolve(resourcePath, locations);
+                if (resolvedResource == null) {
+                    return null;
+                }
+                try {
+                    return resolvedResource.getURL().toString();
+                } catch (IOException e) {
+                    return resolvedResource.getFilename();
+                }
+            }
+
+            private Resource resolve(String requestPath, List<? extends Resource> locations) {
+
+                if (requestPath == null) return null;
+                    return index;
+            }
 
 
 
