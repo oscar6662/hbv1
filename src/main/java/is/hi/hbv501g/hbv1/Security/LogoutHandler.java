c0831f44481a48af0074cfe2be7b853d7226688f
Óskar Helgi <16443844+oscar6662@users.noreply.github.com>
2022-11-23 20:58:38 +0100

    main: logout setup

--- src/main/java/is/hi/hbv501g/hbv1/Security/LogoutHandler.java
+++ src/main/java/is/hi/hbv501g/hbv1/Security/LogoutHandler.java
@@ -51,7 +51,7 @@ public class LogoutHandler extends SecurityContextLogoutHandler {
         String issuer = (String) getClientRegistration().getProviderDetails().getConfigurationMetadata().get("issuer");
         String clientId = getClientRegistration().getClientId();
 //        String returnTo = ServletUriComponentsBuilder.fromCurrentContextPath().build().toString();
-        String returnTo = "http://localhost:5173";
+        String returnTo = "https://hbv1-db.herokuapp.com/home";
 
         String logoutUrl = UriComponentsBuilder
                 .fromHttpUrl(issuer + "v2/logout?client_id={clientId}&returnTo={returnTo}")
