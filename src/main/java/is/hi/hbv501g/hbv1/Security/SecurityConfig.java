e27372632263a5a6dac4324f663a98268c965b4d
Óskar Helgi <16443844+oscar6662@users.noreply.github.com>
2022-11-23 20:03:31 +0100

    ajshdj

--- src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
+++ src/main/java/is/hi/hbv501g/hbv1/Security/SecurityConfig.java
@@ -28,11 +28,13 @@ import org.springframework.web.servlet.resource.ResourceResolverChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    /*private final LogoutHandler logoutHandler;
-
-    public SecurityConfig(LogoutHandler logoutHandler) {
-        this.logoutHandler = logoutHandler;
-    }*/
+    /*
+     * private final LogoutHandler logoutHandler;
+     * 
+     * public SecurityConfig(LogoutHandler logoutHandler) {
+     * this.logoutHandler = logoutHandler;
+     * }
+     */
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -44,9 +46,11 @@ public class SecurityConfig {
                 .permitAll() // allow all users to access the home pages and the static images directory
                 .anyRequest().authenticated() // all other requests must be authenticated
                 .and().oauth2Login().defaultSuccessUrl("https://hbv1-framendi.herokuapp.com/");
-                //.and().logout()
-                //.logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // handle logout requests at /logout path
-                //.addLogoutHandler(logoutHandler) // customize logout handler to log out of Auth0
+        // .and().logout()
+        // .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // handle logout
+        // requests at /logout path
+        // .addLogoutHandler(logoutHandler) // customize logout handler to log out of
+        // Auth0
         return http.build();
     }
 
@@ -87,18 +91,19 @@ class WebConfig implements WebMvcConfigurer {
 
         // Can try to play with
         // registry.addResourceHandler("/**")
-        //        .addResourceLocations("classpath:/static/");
+        // .addResourceLocations("classpath:/static/");
         // But this option can't map every path to index.html
         // Can try https://stackoverflow.com/a/42998817/1032167
         // to resolve this, but then you loose /api/** => rest
         // and to be honest it is some regex madness, so
         // it was easier for me to setup custom resource resolver
 
-
     }
-     class ReactResourceResolver implements ResourceResolver {
+
+    class ReactResourceResolver implements ResourceResolver {
         // this is the same directory you are using in package.json
-        // "build-spring": "react-scripts build &&  mv build static && mv static ../src/main/resources",
+        // "build-spring": "react-scripts build && mv build static && mv static
+        // ../src/main/resources",
         // example REACT_DIR/index.html
         private static final String REACT_DIR = "/static/";
 
@@ -108,34 +113,39 @@ class WebConfig implements WebMvcConfigurer {
         private static final String REACT_STATIC_DIR = "static";
 
         private Resource index = new ClassPathResource(REACT_DIR + "index.html");
+        private List<String> rootStaticFiles = Arrays.asList("vite.svg",
+                "index.6f6ededa.js", "index.8b546473.css");
 
+        @Override
+        public Resource resolveResource(HttpServletRequest request, String requestPath,
+                List<? extends Resource> locations, ResourceResolverChain chain) {
+            return resolve(requestPath, locations);
+        }
 
-            @Override
-            public Resource resolveResource(HttpServletRequest request, String requestPath,
-                                            List<? extends Resource> locations, ResourceResolverChain chain) {
-                System.out.println("is something happening");
+        @Override
+        public String resolveUrlPath(String resourcePath, List<? extends Resource> locations,
+                ResourceResolverChain chain) {
+            Resource resolvedResource = resolve(resourcePath, locations);
+            if (resolvedResource == null) {
+                return null;
+            }
+            try {
+                return resolvedResource.getURL().toString();
+            } catch (IOException e) {
+                return resolvedResource.getFilename();
+            }
+        }
+
+        private Resource resolve(String requestPath, List<? extends Resource> locations) {
+
+            if (requestPath == null) return null;
+
+            if (rootStaticFiles.contains(requestPath)
+                    || requestPath.startsWith(REACT_STATIC_DIR)) {
+                return new ClassPathResource(REACT_DIR + requestPath);
+            } else
                 return index;
-            }
-
-            @Override
-            public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
-                Resource resolvedResource = resolve(resourcePath, locations);
-                if (resolvedResource == null) {
-                    return null;
-                }
-                try {
-                    return resolvedResource.getURL().toString();
-                } catch (IOException e) {
-                    return resolvedResource.getFilename();
-                }
-            }
-
-            private Resource resolve(String requestPath, List<? extends Resource> locations) {
-                if (requestPath == null) return null;
-                    return index;
-            }
-
-
+        }
 
     }
 
